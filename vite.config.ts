6a30e27a068d642b6ecb6a63bbbadc64d95b0fc8
Rets Dima <retsdmitriy@gmail.com>
2024-12-03 23:12:00 +0500

    start

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { ConfigEnv, defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        base: env.VITE_BASE_URL,
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+            },
+        },
+        server: {
+            port: env.VITE_PORT,
+        },
+    };
+});
